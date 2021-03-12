2f75ed2b15f4f63b40605cf78e42e373d9e331ff
shacryt <alex.tsenkilidis@gmail.com>
2021-03-12 13:52:46 +0200

    Updates

---
+++ precache-manifest.ebb07deb4d69d57062b40c1e993870f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2282692d3384d4c6a6d7888c66dfd135",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5075c167872e1c8640ad",
+    "url": "/static/css/main.9deef1be.chunk.css"
+  },
+  {
+    "revision": "bb36db33397ac9985244",
+    "url": "/static/js/2.352c94d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.352c94d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5075c167872e1c8640ad",
+    "url": "/static/js/main.50ae2f55.chunk.js"
+  },
+  {
+    "revision": "0dfbc0eb8637ff182a08",
+    "url": "/static/js/runtime-main.60b7ef7d.js"
+  }
+]);
